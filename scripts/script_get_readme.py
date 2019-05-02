118dbd3c42a3dd4c59dea25f9d35181c52bf02c0
CaioNunes <caiofelipe147@gmail.com>
2019-05-02 15:46:45 -0300

    Correção no script get_readme

--- scripts/script_get_readme.py
+++ scripts/script_get_readme.py
@@ -4,7 +4,6 @@ import urllib
 
 token = os.environ['AUTH_TOKEN']
 
-g = Github(token)
 org = g.get_organization('plataformagames')
 
 for repo in org.get_repos():
@@ -12,7 +11,7 @@ for repo in org.get_repos():
         file_content = repo.get_file_contents('unbgames.md')
 
         if(repo.name != 'unbgames'):
-            urllib.request.urlretrieve(file_content.download_url, ('../readmes/'+repo.name + '.md'))
+            urllib.request.urlretrieve(file_content.download_url, ('readmes/'+repo.name + '.md'))
 
     except Exception as e:
         print("Repository: " + repo.name + " don't have readme.md file.")
